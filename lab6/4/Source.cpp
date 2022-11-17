9bebbad5771c1f47410ebb3476b3b972a7f85b21
Nejern <nejern@yandex.ru>
2022-11-17 15:35:42 +0300

    Added input from file

--- lab6/4/Source.cpp
+++ lab6/4/Source.cpp
@@ -4,16 +4,8 @@
 using namespace std;
 
 int main() {
-  int array[] = {33, 0, 31, 16, 6, 1, 30};
-  int len_array = 7;
-
-  int max = -1;
   int csize = sizeof(int) * 8 - 1;
-  cout << "csize: " << csize << endl;
-  for (int i = 0; i < len_array; i++) {
-    if (max < array[i])
-      max = array[i];
-  }
+  int max = 10000000;
   int len_bits = max / csize + 1;
   int bits[len_bits];
 
@@ -22,8 +14,10 @@ int main() {
   }
   int mask = (1 << (csize - 1));
 
-  for (int i = 0; i < len_array; i++) {
-    int num = array[i];
+  ifstream fIn("./file");
+  while (fIn.eof() == 0) {
+    int num;
+    fIn >> num;
     int cindex = num / csize;
     int offset = num % csize;
     bits[cindex] = (bits[cindex] | (mask >> offset));
